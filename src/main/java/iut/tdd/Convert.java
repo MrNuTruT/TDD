d7442c0c9a251d7cd4febac4cc2acfe8644a0f2c
Paul Dumont <MrNuTruT@gmail.com>
2015-03-30 16:14:40 +0200

    ajout num2text < 17 + *** et un

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -21,12 +21,28 @@ public class Convert {
 		n2t.put("12", "douze");
 		n2t.put("13", "treize");
 		n2t.put("14", "quatorze");
-		n2t.put("15", "qunize");
+		n2t.put("15", "quinze");
 		n2t.put("16", "seize");
+		
+		n2t.put("20", "vingt");
+		n2t.put("30", "trente");
+		n2t.put("40", "quarante");
+		n2t.put("50", "cinquante");
+		n2t.put("60", "soixante");
+		
 	}
 	
 	public static String num2text(String input) {
-		return(n2t.get(input));
+		int nbr = Integer.parseInt(input);
+		if(Integer.parseInt(input)<17 || nbr == 20 || nbr == 30 || nbr == 40 || nbr == 50 || nbr == 60)
+			return(n2t.get(input));
+		
+		else if(nbr == 21 || nbr == 31 || nbr == 41 || nbr == 51 || nbr == 61)
+			return(n2t.get(""+(nbr/10)*10) + " et " + n2t.get(""+(nbr -((nbr/10)*10))));
+					
+		else
+			return(n2t.get(""+(nbr/10)*10) + " " + n2t.get(""+(nbr -((nbr/10)*10))));
+	
 	}
 	public static String text2num(String input) {
 		
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -8,70 +8,111 @@ public class TestConvert {
 	public void test_num2text_zero () {
 		Assert.assertEquals("zéro", Convert.num2text("0"));
 	}
-	
+	@Test
 	public void test_num2text_un () {
 		Assert.assertEquals("un", Convert.num2text("1"));
 	}
-	
+	@Test
 	public void test_num2text_deux () {
 		Assert.assertEquals("deux", Convert.num2text("2"));
 	}
-	
+	@Test
 	public void test_num2text_trois () {
 		Assert.assertEquals("trois", Convert.num2text("3"));
 	}
-	
+	@Test
 	public void test_num2text_quatre () {
 		Assert.assertEquals("quatre", Convert.num2text("4"));
 	}
-	
+	@Test
 	public void test_num2text_cinq () {
 		Assert.assertEquals("cinq", Convert.num2text("5"));
 	}
-	
+	@Test
 	public void test_num2text_six () {
 		Assert.assertEquals("six", Convert.num2text("6"));
 	}
-	
+	@Test
 	public void test_num2text_sept () {
 		Assert.assertEquals("sept", Convert.num2text("7"));
 	}
-	
+	@Test
 	public void test_num2text_huit () {
 		Assert.assertEquals("huit", Convert.num2text("8"));
 	}
-	
+	@Test
 	public void test_num2text_neuf () {
 		Assert.assertEquals("neuf", Convert.num2text("9"));
 	}
-	
+	@Test
 	public void test_num2text_dix () {
 		Assert.assertEquals("dix", Convert.num2text("10"));
 	}
-	
+	@Test
 	public void test_num2text_onze () {
 		Assert.assertEquals("onze", Convert.num2text("11"));
 	}
-	
+	@Test
 	public void test_num2text_douze () {
 		Assert.assertEquals("douze", Convert.num2text("12"));
 	}
-	
+	@Test
 	public void test_num2text_treize () {
 		Assert.assertEquals("treize", Convert.num2text("13"));
 	}
-	
+	@Test
 	public void test_num2text_quatorze () {
 		Assert.assertEquals("quatorze", Convert.num2text("14"));
 	}
-	
+	@Test
 	public void test_num2text_quinze () {
 		Assert.assertEquals("quinze", Convert.num2text("15"));
 	}
-	
+	@Test
 	public void test_num2text_seize () {
 		Assert.assertEquals("seize", Convert.num2text("16"));
 	}
 	
+	// 10 
+	@Test
+	public void test_num2text_vingt () {
+		Assert.assertEquals("vingt", Convert.num2text("20"));
+	}
+	@Test
+	public void test_num2text_trente () {
+		Assert.assertEquals("trente", Convert.num2text("30"));
+	}
+	@Test
+	public void test_num2text_quarante () {
+		Assert.assertEquals("quarante", Convert.num2text("40"));
+	}
+	@Test
+	public void test_num2text_cinquante () {
+		Assert.assertEquals("cinquante", Convert.num2text("50"));
+	}
+	@Test
+	public void test_num2text_soixante () {
+		Assert.assertEquals("soixante", Convert.num2text("60"));
+	}
 	
+	@Test
+	public void test_num2text_vingt_et_un () {
+		Assert.assertEquals("vingt et un", Convert.num2text("21"));
+	}
+	@Test
+	public void test_num2text_trente_et_un () {
+		Assert.assertEquals("trente et un", Convert.num2text("31"));
+	}
+	@Test
+	public void test_num2text_quarante_et_un () {
+		Assert.assertEquals("quarante et un", Convert.num2text("41"));
+	}
+	@Test
+	public void test_num2text_cinquante_et_un () {
+		Assert.assertEquals("cinquante et un", Convert.num2text("51"));
+	}
+	@Test
+	public void test_num2text_soixante_et_un () {
+		Assert.assertEquals("soixante et un", Convert.num2text("61"));
+	}
 }
